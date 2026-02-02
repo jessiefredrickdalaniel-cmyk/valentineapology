87e90ebea4b30c9469223ace601ffae4701c18b7
jessiefredrickdalaniel-cmyk <jessiefredrickdalaniel@gmail.com>
2026-02-02 11:48:51 +0800

    personalized

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
+    "No jud?",
+    "Final na?",
+    "Free hugs by..",
+    "Libre tika..",
+    "Ikaw manglibre?",
+    "Free hugs, kisses, plus libre laag",
+    "Sorry na wuvv u!",
+    "Dili juuuuuddd?",
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
 ];
